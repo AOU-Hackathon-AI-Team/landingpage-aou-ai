9f12d8a9b4cf6b93128bb94ea9e51b5c1268f08d
saleh alsaggaf <saa111954@hotmail.com>
2022-12-03 13:41:11 +0300

    update: hero banner

--- shared/js/heroBanner.js
+++ shared/js/heroBanner.js
@@ -1,9 +1,4 @@
 const heroBanner = `
-<picture>
-            <img src="${window.location.origin}/shared/image/hero.png">
-        </picture>
-  
-  
         <div class="main_text">
             <p>
                 عن هاكاثون عن هاكاثون عن هاكاثون
@@ -13,8 +8,9 @@ const heroBanner = `
                 شارك الان
             </button>
         </div>
-
+        <picture>
+        <img src="${window.location.origin}/shared/image/hero.png">
+    </picture>
+       
 `;
-console.log('location ==> ',window.location);
-console.log('location ==> ',`${window.location.origin}/shared/image/hero.png`);
 document.getElementById('heroBannar').innerHTML = heroBanner;
