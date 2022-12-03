f887ac410d67ee2c7679461e2474d2c29bada53f
Ahmed525-12 <ahmedsayed5250057@gmail.com>
2022-12-04 02:14:42 +0300

    second commit

---
+++ shared/js/prizes.js
@@ -0,0 +1,21 @@
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
+
+  document.getElementById("price").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card-p")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
