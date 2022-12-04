307d8c3da633d116c9ab5dec2e207dad97b0709b
saleh alsaggaf <saa111954@hotmail.com>
2022-12-05 02:22:51 +0300

    task: added footer

---
+++ shared/js/footer.js
@@ -0,0 +1,33 @@
+
+
+document.getElementById('footer').innerHTML = 
+
+
+`
+
+<div class="footer wrapper">
+
+        <picture>
+            <img src="${href}/shared/image/logo.svg">
+        </picture>
+
+        <div>
+            <p>من منطلق تحقيق أهداف التنمية المستدامة في الذكاء الاصطناعي، والذي يلعب دورًا مهمًا ومركزيًا في تحقيق مستهدفات رؤية السعودية 2030، أطلقت الجامعة العربية المفتوحة هاكاثون(...)؛ وذلك لتمكين طلبة الجامعات السعودية من تقديم حلول تقنية، وأفكار إبداعية، تواكب التحول التقني بتطبيق الذكاء الاصطناعي في مختلف مجالات التحدي.
+            </p>
+            <nav>
+                <a href="#" class="active">الرئيسية</a>
+                <a href="#">مسارات التحدي</a>
+                <a href="#">الجدول الزمني</a>
+                <a href="#">الجوائز</a>
+                <a href="#">شركائنا</a>
+                <a href="#">الاسئلة الشائعه</a>
+            </nav>
+        </div>
+        <div>
+            <a><img src="${href}/shared/icon/linkedin.svg"></a>
+            <a><img src="${href}/shared/icon/twitter.svg"></a>
+            <a><img src="${href}/shared/icon/youtube.svg"></a>
+        </div>
+
+
+`;
